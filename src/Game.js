55a246d19d62f80377353971526e5c50b90744d0
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-08-18 19:31:48 -0300

    Added choose screen

---
+++ src/Game.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Game.css';
+
+class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number_of_cells: 0,
+            text: '',
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <h1>Searching Game</h1>
+                <label htmlFor="new-todo">
+                    How many cells?
+                </label>
+                <br/>
+                <input
+                    id="new-todo"
+                    onChange={this.handleChange}
+                    value={this.state.text}
+                    style={{
+                        marginTop: 10
+                    }}
+                />
+                <button>
+                    Go!
+                </button>
+                
+            </div>
+        )
+    };
+
+    handleChange(e) {
+        if (!isNaN(e.target.value)) {
+            this.setState({
+                text: e.target.value,
+            });
+        }
+    }
+
+    handleSubmit(e) {
+    }
+}
+
+export default Game;
